f32f71f8e1aa9a24482132cbd45486d19d9f6d0c
Maria Sharova <sharova.maria.3d@gmail.com>
2023-03-30 17:08:08 +0200

    added working form with input to search for different words

---
+++ src/Dictionary.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+
+export default function Dictionary() {
+    let [searchWord, setSearchWord] = useState("");
+
+    function searchHandle(event) {
+        event.preventDefault();
+        alert(`The user is searching for "${searchWord}" definition`);
+    }
+    function changeHandle(event) {
+        setSearchWord(event.target.value);
+    }
+    return (
+        <section className="Dictionary">
+            <form onSubmit={searchHandle}>
+                <input 
+                    type="search" 
+                    placeholder="Search for any word here..." 
+                    className="w-50"
+                    onChange={changeHandle}></input>
+            </form>
+        </section>
+    );
+}
