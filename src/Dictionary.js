f25c7c02e8b1d0aa569f0af748c1aadd4c444548
Maria Sharova <sharova.maria.3d@gmail.com>
2023-03-31 15:01:24 +0200

    integrated api dictionary response for words

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,9 +6,17 @@ import "./Dictionary.css";
 export default function Dictionary() {
     let [searchWord, setSearchWord] = useState("");
 
+    function handleApi(response) {
+        console.log(response.data.meanings);
+    }
+
     function searchHandle(event) {
         event.preventDefault();
-        alert(`The user is searching for "${searchWord}" definition`);
+        //alert(`The user is searching for "${searchWord}" definition`);
+        let apiKey = "70bb37b84aobtb35a69f8896391b01b3";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchWord}&key=${apiKey}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(handleApi);
     }
     function changeHandle(event) {
         setSearchWord(event.target.value);
